0c898b796751071a049e6884bcdcd5eb4d077a06
Yakshit <yakshitgarg25@gmail.com>
2024-08-05 11:55:35 +0530

    refactor: content

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='fixed bottom-0 left-0 right-0 pb-6 z-10 text-lg font-medium text-center'>Optmyzr Tech Pvt. Ltd</footer>
+    <footer className='fixed bottom-0 left-0 right-0 pb-6 z-10 text-lg font-medium text-center'>Made With Love</footer>
   )
 }
 
